596f9fe4e4d4e96368b49a63c9ee590202397f8b
Collin D. Bevins <cbevins@montana.com>
2019-06-03 18:17:17 -0600

    Implemented ConfigPage

--- src/components/ConfigPage.js
+++ src/components/ConfigPage.js
@@ -1,22 +1,88 @@
 import React from 'react';
-import {KeyedList, keyedListValueCompare} from './KeyedList';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
-export function ConfigList(props) {
-  const items = [];
-  props.dag.requiredConfigLeafs.forEach((leaf) => {
-    let name = leaf.fullName('/');
-    items.push({key: name, value: name});
-  });
-  items.sort(keyedListValueCompare);
-  const selected = props.dag.selectedLeafs.length;
+function configChanged(dag, leaf, e) {
+  //alert(e.target.value);
+  dag.setValue(leaf, e.target.value);
+}
+
+function ConfigOption(props) {
+  const label = props.leaf.own.option.items[props.value];
+  const current = props.leaf.own.value;
+  if (current === props.value) {
+    return <option value={props.value} selected>{label}</option>;
+  } else {
+    return <option value={props.value}>{label}</option>;
+  }
+}
+
+function ConfigItem(props) {
+  const leaf = props.leaf;
+  const items = Object.keys(leaf.own.option.items);
+  const options = items.map((item) =>
+    <ConfigOption leaf={leaf} value={item} />
+  );
+  return (
+    <Form.Control as="select"
+      onChange={(e) => configChanged(props.dag, leaf, e)}>
+      {options}
+    </Form.Control>
+  );
+}
+
+function ConfigLabel(props) {
+  return (
+    <Form.Label>{props.leaf.own.option.header}</Form.Label>
+  );
+}
+
+function ConfigList(props) {
+  const block = props.block;
+  const configs = Object.keys(block);
+  const items = configs.map((config) =>
+    <Form.Group controlId="formConfigPage">
+      <ConfigLabel leaf={block[config]} />
+      <ConfigItem dag={props.dag} leaf={block[config]}/>
+    </Form.Group>
+  );
   return (
     <div>
-      <h3>The {selected} selected variables have {items.length} active configurations:</h3>
-      <KeyedList items={items} />
+      {items}
     </div>
   );
 }
 
+function ConfigBlock(props) {
+  const block = props.block;  // parent branch of the block config leafs
+  const title = block.fullName();
+  return (
+    <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Card.Header} variant="link"
+          eventKey={props.ekey}>
+        {title}
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey={props.ekey}>
+      <Card.Body>
+        <ConfigList dag={props.dag} block={block} />
+      </Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  );
+}
+
 export default function ConfigPage(props) {
-  return <ConfigList dag={props.dag} />
+  const { fuel, slope, fire, wind, crown } = props.dag.tree.configs;
+  return (
+    <Accordion defaultActiveKey="0">
+      <ConfigBlock dag={props.dag} block={fuel} ekey="0" />
+      <ConfigBlock dag={props.dag} block={slope} ekey="1" />
+      <ConfigBlock dag={props.dag} block={fire} ekey="2" />
+      <ConfigBlock dag={props.dag} block={wind} ekey="3" />
+      <ConfigBlock dag={props.dag} block={crown} ekey="4" />
+    </Accordion>
+  );
 }
--- src/components/SelectPage.js
+++ src/components/SelectPage.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+
+function surfacePrimary(dag) {
+  const root = dag.tree.surface.fuel.primary;
+  const prefix = 'Primary Surface Fuel ';
+  return {
+    title: prefix + 'Fire Behavior',
+    leafs: [
+      [root.fire.ros, prefix+'Fire Spread Rate'],
+      [root.fire.firelineIntensity, prefix+'Fireline Intensity'],
+      [root.fire.flameLength, prefix+'Flame Length'],
+    ],
+  }
+}
+
+function surfaceSecondary(dag) {
+  const root = dag.tree.surface.fuel.secondary;
+  const prefix = 'Secondary Surface Fuel ';
+  return {
+    title: prefix + 'Fire Behavior',
+    leafs: [
+      [root.fire.ros, prefix+'Fire Spread Rate'],
+      [root.fire.firelineIntensity, prefix+'Fireline Intensity'],
+      [root.fire.flameLength, prefix+'Flame Length'],
+    ],
+  }
+}
+
+function surfaceEllipse(dag) {
+  const root = dag.tree.surface.fire.ellipse;
+  const prefix = 'Surface Fire Ellipse ';
+  return {
+    title: prefix + 'Size and Shape',
+    leafs: [
+      [root.axis.lengthToWidthRatio, prefix+'Length-to-Width Ratio'],
+      [root.size.area, prefix+'Area'],
+      [root.size.perimeter, prefix+'Perimeter'],
+    ],
+  }
+}
 
 function selectionMade(dag, leaf, e) {
   if (e.target.checked) {
@@ -9,32 +50,49 @@ function selectionMade(dag, leaf, e) {
   }
 }
 
-export function SelectionList(props) {
-  const { primary } = props.dag.tree.surface.fuel;
-  const selectableLeafs = [
-    primary.fire.ros,
-    primary.fire.firelineIntensity,
-    primary.fire.flameLength,
-  ];
-
-  const items = selectableLeafs.map((leaf) =>
+function SelectionList(props) {
+  const items = props.leafs.map((item) =>
     <Form.Check type="checkbox"
-      onClick={(e) => selectionMade(props.dag, leaf, e)}
-      id={leaf.fullName('-')}
-      label={leaf.name()} />
+      onClick={(e) => selectionMade(props.dag, item[0], e)}
+      id={item[0].fullName('-')}
+      label={`${item[1]} (${item[0].name()})`} />
   );
   return (
     <div>
-      <h3>Select from the following {items.length} variables:</h3>
       {items}
     </div>
   );
 }
 
-export default function SelectPage(props) {
+function SelectBlock(props) {
   return (
-    <Form.Group controlId="formBasicChecbox">
-      <SelectionList dag={props.dag} />
-    </Form.Group>
+    <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Card.Header} variant="link"
+          eventKey={props.ekey}>
+        {props.data.title}
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey={props.ekey}>
+      <Card.Body>
+        <Form.Group controlId="formSelectPage">
+          <SelectionList dag={props.dag} leafs={props.data.leafs} />
+        </Form.Group>
+      </Card.Body>
+    </Accordion.Collapse>
+  </Card>
+  );
+}
+
+export default function SelectPage(props) {
+  const sp = surfacePrimary(props.dag);
+  const ss = surfaceSecondary(props.dag);
+  const fe = surfaceEllipse(props.dag);
+  return (
+    <Accordion defaultActiveKey="0">
+      <SelectBlock dag={props.dag} data={sp} ekey="0" />
+      <SelectBlock dag={props.dag} data={ss} ekey="1" />
+      <SelectBlock dag={props.dag} data={fe} ekey="2" />
+    </Accordion>
   );
 }
